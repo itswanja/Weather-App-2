a15ea5a6a0b2690e50e6614790328f9217473ec5
Charity Mwaura <wanjacharity92@gmail.com>
2024-08-21 12:16:49 +0300

    Added API Integration

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function fetchWeather(response) {
+  let locationElement = document.querySelector("#location");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+
+  let temperature = response.data.temperature.current;
+  locationElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  temperatureElement.innerHTML = `${Math.round(temperature)} &deg;C`;
+}
+
+function searchLocation(location) {
+  let apiKey = "o644f30b4cbteb80ba67a353f5909861";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${location}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(fetchWeather);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#locationInput");
+
+  searchLocation(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearch);
